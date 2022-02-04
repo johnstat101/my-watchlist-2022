ce04976bc810c0ec0357b2ed410b955e9b8b1831
Rea <rmrea@kenya-airways.com>
2022-02-04 23:15:39 +0300

    add dabase

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export MOVIE_API_KEY='c662f6ba064a7eee41a8cc9ed2e1ff59'
 export SECRET_KEY='<Flask WTF Secret Key>'
+export MAIL_USERNAME='johnjorogek@gmail.com'
+export MAIL_PASSWORD='Ms261-0020/2010'
 
 python manage.py server
